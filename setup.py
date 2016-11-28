7310c513409c4e2d0108d7c7c7ef9b46a5a6ea4b
David Huser <david.huser@unibas.ch>
2016-11-28 12:17:07 +0100

    fix tests

--- setup.py
+++ setup.py
@@ -1,10 +1,17 @@
 from setuptools import setup, find_packages
 
+_VERSION_ = '0.1'
+
 setup(name='commcare-cases-dhis2-events',
-      version='0.1',
+      version=_VERSION_,
       description='Extract cases from CommCareHQ and post them as events to DHIS2',
       author='Swiss Tropical and Public Health Institute (Swiss TPH)',
       author_email='david.huser@unibas.ch',
       url='https://github.com/SwissTPH/commcare-cases-dhis2-events',
-      packages=find_packages()
+      packages=find_packages(),
+      install_requires=[
+            'requests>=2.11.1',
+            'python-dateutil>=2.5.3',
+            'pytest>=3.0.4'
+      ]
       )
