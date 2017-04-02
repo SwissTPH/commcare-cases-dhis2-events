9de93cfcc9fc4d6244a7f44f93e6fe70e78a2c33
David Huser <davidhuser303@bitbucket.com>
2017-04-02 14:23:18 +0200

    refactored module, fixed bug for timewindows calling at midnight

--- app/timewindow.py
+++ app/timewindow.py
@@ -18,19 +18,10 @@ class Timewindow:
         return json.dumps(self.window)
 
 
-def create_timewindow(start, end):
-    """
-    creating tuples with starttime and endtime. Input is day and hour, eg. <2016-03-03> and <15>
-    Example return: (2016-03-03T15:00:00Z, 2016-03-03T15:59:59Z)
-    """
-    s = "{}T{}".format(start.strftime("%Y-%m-%d"), start.strftime("%H:%M:%S"))
-    e = "{}T{}".format(end.strftime("%Y-%m-%d"), end.strftime("%H:%M:%S"))
-    return Timewindow(s, e)
-
-
 def get_windows_from_start_to_end(start_ts, end_ts):
     """ creating a list of window strings from a starting date until the last full hour"""
     window_list = []
+    time_format = '%Y-%m-%dT%H:%M:%S'
     index = start_ts
     while index < end_ts:
         s = index
@@ -38,7 +29,7 @@ def get_windows_from_start_to_end(start_ts, end_ts):
         minutes_diff = (end_ts - index).total_seconds() / 60.0
         if minutes_diff < 60.0:
             e = end_ts
-        tw = create_timewindow(s, e)
+        tw = Timewindow(start=s.strftime(time_format), end=e.strftime(time_format))
         window_list.append(tw)
         index = index + datetime.timedelta(hours=1)
     return window_list
@@ -46,18 +37,7 @@ def get_windows_from_start_to_end(start_ts, end_ts):
 
 def get_window_of_lasthour(datetimenow):
     """ timewindow strings of last hour, XX:00:00 - XX:59:59 where XX = NOW.hour-1 """
-    # round <now> down to the full hour minus 1 second, strip to XX:59:59 format
-    last_hour_fiftynine_fiftynine = str(datetimenow - datetime.timedelta(minutes=datetimenow.minute,
-                                                                         seconds=datetimenow.second + 1,
-                                                                         microseconds=datetimenow.microsecond))[11:]
-    # subtract one hour from now, round down to the full hour, strip to XX:00:00 format
-    last_hour_zerozero = str(datetimenow - datetime.timedelta(minutes=datetimenow.minute + 60,
-                                                              seconds=datetimenow.second,
-                                                              microseconds=datetimenow.microsecond))[11:]
-
-    # add T and Z in between
-    date_today = datetimenow.strftime("%Y-%m-%d")
-    start = date_today + "T" + last_hour_zerozero + "Z"
-    end = date_today + "T" + last_hour_fiftynine_fiftynine + "Z"
-    window = Timewindow(start, end)
-    return window
+    before = (datetimenow - datetime.timedelta(hours=1))
+    start = before.strftime("%Y-%m-%dT%H:00:00")
+    end = before.strftime("%Y-%m-%dT%H:59:59")
+    return Timewindow(start, end)
